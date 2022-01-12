c933407a4f91cf9f53ca897feb10f01f183f494e
Sorrell Sohota <sjs@Sorrells-MBP-2.lan>
2022-01-12 12:17:56 +0000

    fixed index page bug

--- src/index.js
+++ src/index.js
@@ -2,7 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import helmet from './assets/img/sword-1.png'
 
 ReactDOM.render(
   <React.StrictMode>
